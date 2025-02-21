64dac9b4744e1d641984c45200b4efa516c7dda3
unknown <rido@gmail.com>
2025-02-21 19:54:55 +0800

    refining table

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(
     cors({
         origin: "*", // Allow all domains (replace with specific origins if needed)
         methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
-        allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers in requests
+        allowedHeaders: ["Content-Type", "Authorization", "adminAuth"], // Allowed headers in requests
     })
 );
 
@@ -30,9 +30,7 @@ const initializeDatabase = async () => {
                 id INT AUTO_INCREMENT PRIMARY KEY,
                 email VARCHAR(255) UNIQUE NOT NULL,
                 password VARCHAR(255) NOT NULL,
-                name VARCHAR(255) NOT NULL,
-                phone VARCHAR(255) NOT NULL,
-                role ENUM('owner', 'employee','admin') NOT NULL,
+                created_by VARCHAR(255) NOT NULL,
                 updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
             )
@@ -41,10 +39,36 @@ const initializeDatabase = async () => {
             CREATE TABLE IF NOT EXISTS shop (
                 id INT AUTO_INCREMENT PRIMARY KEY,
                 shop_name VARCHAR(255) NOT NULL,
-                updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                 account_id INT NOT NULL,
-                CONSTRAINT fk_owner FOREIGN KEY (account_id) REFERENCES account(id) ON DELETE RESTRICT
+                CONSTRAINT fk_shop_account_id FOREIGN KEY (account_id) REFERENCES account(id) ON DELETE RESTRICT,
+                updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )
+        `);
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS owner (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                name VARCHAR(255) NOT NULL,
+                phone VARCHAR(255) NOT NULL,
+                account_id INT NOT NULL,
+                CONSTRAINT fk_owner_account_id FOREIGN KEY (account_id) REFERENCES account(id) ON DELETE RESTRICT,
+                updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )
+        `);
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS employee (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                name VARCHAR(255) NOT NULL,
+                phone VARCHAR(255) NOT NULL,
+                owner_id INT NOT NULL,
+                shop_id INT NOT NULL,
+                account_id INT NOT NULL,
+                CONSTRAINT fk_employee_account_id FOREIGN KEY (account_id) REFERENCES account(id) ON DELETE RESTRICT,
+                CONSTRAINT fk_employee_shop_id FOREIGN KEY (shop_id) REFERENCES shop(id) ON DELETE RESTRICT,
+                CONSTRAINT fk_employee_owner_id FOREIGN KEY (owner_id) REFERENCES owner(id) ON DELETE RESTRICT,
+                updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
             )
         `);
         await pool.query(`
@@ -54,7 +78,9 @@ const initializeDatabase = async () => {
                 product_quantity INT NOT NULL,
                 product_price INT NOT NULL,
                 shop_id INT NOT NULL,
-                CONSTRAINT fk_shop_product FOREIGN KEY (shop_id) REFERENCES shop(id) ON DELETE RESTRICT,
+                CONSTRAINT fk_product_shop FOREIGN KEY (shop_id) REFERENCES shop(id) ON DELETE RESTRICT,
+                created_by INT NOT NULL,
+                CONSTRAINT fk_product_created_by FOREIGN KEY (created_by) REFERENCES account(id) ON DELETE RESTRICT,
                 updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
             )
@@ -63,19 +89,20 @@ const initializeDatabase = async () => {
         await pool.query(`
             CREATE TABLE IF NOT EXISTS transaction (
                 id INT AUTO_INCREMENT PRIMARY KEY,
-                product_id INT NOT NULL,
-                account_id INT NOT NULL,
                 customer_phone VARCHAR(255) NOT NULL,
                 customer_name VARCHAR(255) NOT NULL,
                 customer_product VARCHAR(255) NOT NULL,
+                created_by INT NOT NULL,
                 total_price INT NOT NULL,
                 status ENUM('success', 'failed') NOT NULL,
                 shop_id INT NOT NULL,
                 CONSTRAINT fk_shop_transaction FOREIGN KEY (shop_id) REFERENCES shop(id) ON DELETE RESTRICT,
-                updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                account_id INT NOT NULL,
+                CONSTRAINT fk_transaction_created_by FOREIGN KEY (created_by) REFERENCES account(id) ON DELETE RESTRICT,
+                product_id INT NOT NULL,
                 CONSTRAINT fk_product FOREIGN KEY (product_id) REFERENCES product(id) ON DELETE RESTRICT,
-                CONSTRAINT fk_employee FOREIGN KEY (account_id) REFERENCES account(id) ON DELETE RESTRICT
+                updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
             )
         `);
         console.log("✅ Database initialized successfully!");
